9ef73037bd50b2293be76adc61de99c8b98901fc
SRI VAISHNAVI V <vaishnavivaradharaj0@gmail.com>
2026-09-09 10:24:30 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,17 @@
+use vaishudb;
 
+CREATE TABLE Student(
+ SudentID INT(5) PRIMARY KEY,
+ Studentname VARCHAR(20) NOT
+ NULL,
+  DOB DATE NOT NULL,
+  Genderb VARCHAR(10)NOT NULL,
+  DepartmentID INT(5),
+  CONSTRAINT UQ_StudentName
+UNIQUE(studentName),
+  CONSTRAINT FK_department
+FOREIGN KEY(DepartmentID)
+  REFERENCES
+department(DepartmentID)
+);
+  
